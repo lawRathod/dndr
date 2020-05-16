def6a6316fda7dbbcae29f31574daad2135efd17
Prateek <emaillawrathod@gmail.com>
2020-05-17 03:26:51 +0530

    initial commint, working pdf viewer

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_full_pdf_viewer/full_pdf_viewer_scaffold.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+const String _documentPath = '/storage/emulated/0/Download/check.pdf';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Opening a PDF',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // This moves the PDF file from the assets to a place accessible by our PDF viewer.
+
+  final Permission x = Permission.storage;
+
+  Future<void> requestPermission(Permission permission) async {
+    final status = await permission.request();
+  }
+
+  Future<String> prepareTestPdf() async {
+    final ByteData bytes =
+        await DefaultAssetBundle.of(context).load(_documentPath);
+    final Uint8List list = bytes.buffer.asUint8List();
+
+    final tempDir = await getTemporaryDirectory();
+    final tempDocumentPath = '${tempDir.path}/$_documentPath';
+
+    final file = await File(tempDocumentPath).create(recursive: true);
+    file.writeAsBytesSync(list);
+    return tempDocumentPath;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Opening a PDF"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () => {
+                // We need to prepare the test PDF, and then we can display the PDF.
+                prepareTestPdf().then((path) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => FullPdfViewerScreen(path)),
+                  );
+                })
+              },
+              child: const Text('Open PDF with full_pdf_viewer'),
+            ),
+            RaisedButton(
+              onPressed: () => {
+                // We need to prepare the test PDF, and then we can display the PDF.
+                requestPermission(x)
+              },
+              child: const Text('Request Permission'),
+              
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FullPdfViewerScreen extends StatelessWidget {
+  final String pdfPath;
+
+  FullPdfViewerScreen(this.pdfPath);
+
+  @override
+  Widget build(BuildContext context) {
+    return PDFViewerScaffold(
+        appBar: AppBar(
+          title: Text("Document"),
+        ),
+        path: pdfPath);
+  }
+}
