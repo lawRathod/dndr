6f78d8bd2c949db32ad341acc37ade8eb98edcda
Prateek <emaillawrathod@gmail.com>
2020-06-17 01:07:26 +0530

    added search feature and list organization changes

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,15 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final ScrollController _scrollController = ScrollController();
   List<FileSystemEntity> _pdfs = [];
+  List<FileSystemEntity> _dup = [];
   Permission storagePerm = Permission.storage;
   PermissionStatus status = PermissionStatus.undetermined;
+  TextEditingController editingController = TextEditingController();
 
   @override
   void initState() {
-    checkStatus(storagePerm);
     super.initState();
+    checkStatus(storagePerm);
   }
 
   void requestPermission(Permission permission) async {
@@ -31,6 +33,7 @@ class _MyAppState extends State<MyApp> {
   }
 
   void getlists() {
+    _pdfs.clear();
     Directory dir = Directory('/storage/emulated/0/');
     // String pdfdir = dir.toString();
     List<FileSystemEntity> _files;
@@ -42,16 +45,47 @@ class _MyAppState extends State<MyApp> {
         _pdfs.add(entity);
       }
     }
-    _pdfs = new List.from(_pdfs.reversed);
+    _pdfs.sort((a, b) {
+      return basename(a.path)
+          .replaceAll(".pdf", "")
+          .compareTo(basename(b.path).replaceAll(".pdf", ""));
+    });
+
+    _dup = new List.from(_pdfs);
+  }
+
+  void filterSearchResults(String query) {
+    List<FileSystemEntity> dummySearchList = List<FileSystemEntity>();
+    dummySearchList.addAll(_dup);
+    if (query.isNotEmpty) {
+      List<FileSystemEntity> dummyListData = List<FileSystemEntity>();
+      dummySearchList.forEach((item) {
+        if (basename(item.path).replaceAll(".pdf", "").contains(query)) {
+          dummyListData.add(item);
+        }
+      });
+      setState(() {
+        _pdfs.clear();
+        _pdfs.addAll(dummyListData);
+      });
+      return;
+    } else {
+      setState(() {
+        _pdfs.clear();
+        _pdfs.addAll(_dup);
+      });
+    }
   }
 
   void checkStatus(Permission permission) async {
-    status = (await permission.status);
+    status = await permission.status;
     if (status != PermissionStatus.granted) {
       requestPermission(permission);
     } else {
       print(status);
     }
+
+    getlists();
   }
 
   Future navigateToSubPage(context, pdf) async {
@@ -59,54 +93,90 @@ class _MyAppState extends State<MyApp> {
         context, MaterialPageRoute(builder: (context) => PDFscreen(pdf)));
   }
 
-  Widget _buildrow(a, context) {
-    return ListTile(
-      title:
-          a is String ? Text(a) : Text(basename(a.path).replaceAll(".pdf", "")),
-      onTap: () {
-        navigateToSubPage(context, a.path.toString());
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    getlists();
     return MaterialApp(
         title: "Home",
+        theme: ThemeData(
+          primaryColor: Colors.black,
+          accentColor: Colors.black,
+
+        ),
+        
         home: Scaffold(
             appBar: AppBar(
               title: Text("dndr"),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.refresh),
+                  onPressed: () {
+                    getlists();
+                  },
+                )
+              ],
               backgroundColor: Colors.black,
             ),
             floatingActionButton: FloatingActionButton(
                 backgroundColor: Colors.black,
                 onPressed: () {},
-                child: Icon(Icons.settings)),
-            body: DraggableScrollbar.semicircle(
-              // labelTextBuilder: (double offset) => Text("${offset ~/ 100}"),
-              controller: _scrollController,
-              child: ListView.builder(
-                controller: _scrollController,
-                itemCount: _pdfs.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    padding: EdgeInsets.fromLTRB(3, 0, 3, 0),
-                    child: InkWell(
-                      child: Card(
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(color: Colors.black, width: 1)),
-                          child: _buildrow(_pdfs[index], context),
-                        ),
-                      ),
+                child: Icon(Icons.search)),
+            body: Container(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      onChanged: (value) {
+                        filterSearchResults(value);
+                      },
+                      controller: editingController,
+                      decoration: InputDecoration(
+                          labelText: "Search",
+                          hintText: "Type Something",
+                          prefixIcon: Icon(Icons.search),
+                          border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(25.0)))),
                     ),
-                  );
-                },
+                  ),
+                  Expanded(
+                      child: DraggableScrollbar.semicircle(
+                    // labelTextBuilder: (double offset) => Text("${offset ~/ 100}"),
+                    controller: _scrollController,
+                    child: ListView.builder(
+                      controller: _scrollController,
+                      itemCount: _pdfs.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                            padding: EdgeInsets.fromLTRB(3, 0, 3, 0),
+                            child: Container(
+                              child: InkWell(
+                                borderRadius: BorderRadius.all(Radius.circular(100)),
+                                child: Card(
+                                  elevation: 0,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10)),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        border: Border.all(
+                                            color: Colors.black, width: 1)),
+                                    child: ListTile(
+                                      title: Text(basename(_pdfs[index].path)
+                                          .replaceAll(".pdf", "")),
+                                      onTap: () {
+                                        navigateToSubPage(context,
+                                            _pdfs[index].path.toString());
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ));
+                      },
+                    ),
+                  )),
+                ],
               ),
             )));
   }
